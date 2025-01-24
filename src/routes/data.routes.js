1668122331a095245f5d092b85fbe99323d94dc3
Shekhar-rana007 <ranashekhar007@gmail.com>
2025-01-24 15:49:17 +0530

    some change

--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -20,6 +20,6 @@ routes.route('/get').get(Authentication, getAllData);
 routes.route('/delete/:id').delete(Authentication, DeteleOneData);
 routes.route('/update/:id').get(Authentication, updateOneData);
 routes.route("/total-data-count").get(Authentication,DataCounsts)
-routes.route("/vulnerableItems").get(vulnerableItems)
+routes.route("/vulnerableItems").get(Authentication,vulnerableItems)
 
 export default routes;
