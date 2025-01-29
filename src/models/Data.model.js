12bafbe5117b6977aa45bd0b08ad930c04b1e3aa
Shekhar-rana007 <ranashekhar007@gmail.com>
2025-01-29 17:57:49 +0530

    some change

--- src/models/Data.model.js
+++ src/models/Data.model.js
@@ -6,7 +6,7 @@ const DataSchema = new Schema(
     Application_Name: { type: String },
     Title: { type: String },
     Vulnerability_Classification: { type: String },
-    Assigned_To: { type: Schema.Types.ObjectId,ref:"User" },
+    Assigned_To: { type:String },
     Scan_Type: { type: String },
     Severity: { type: String },
     Priority: { type: String },
