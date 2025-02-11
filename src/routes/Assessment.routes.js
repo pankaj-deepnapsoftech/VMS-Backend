5213a6a69563e3fbc61d161e0f20f9e0ee548c0a
Shekhar-rana007 <ranashekhar007@gmail.com>
2025-02-11 17:58:30 +0530

    some changes

--- src/routes/Assessment.routes.js
+++ src/routes/Assessment.routes.js
@@ -1,15 +1,15 @@
 import { Router } from 'express';
 import { createAssessment, deleteAssessment, getAssessment, tasterList, updateAssessment } from '../controller/Assessment.controller.js';
-import { Authentication, ClientSMEAuthentication } from '../middleware/Authentication.js';
+import { Authentication } from '../middleware/Authentication.js';
 import { upload } from '../config/multer.config.js';
 import { Assessmentvalidater } from '../helper/helper.js';
 
 const router = Router();
 
-router.route('/create').post(ClientSMEAuthentication,upload.single('code_Upload'),Assessmentvalidater, createAssessment);
-router.route('/get').get(ClientSMEAuthentication, getAssessment);
-router.route('/delete/:id').delete(ClientSMEAuthentication, deleteAssessment);
-router.route('/update/:id').patch(ClientSMEAuthentication, upload.single('code_Upload'), updateAssessment);
-router.route("/testers-list").get(Authentication,tasterList)
+router.route('/create').post(Authentication, upload.single('code_Upload'), Assessmentvalidater, createAssessment);
+router.route('/get').get(Authentication, getAssessment);
+router.route('/delete/:id').delete(Authentication, deleteAssessment);
+router.route('/update/:id').patch(Authentication, upload.single('code_Upload'), updateAssessment);
+router.route('/testers-list').get(Authentication, tasterList);
 
 export default router;
