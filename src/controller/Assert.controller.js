6724470271a80b4dade1f6d661cdbfb99d9018fa
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-05-22 15:54:25 +0530

    some change

--- src/controller/Assert.controller.js
+++ src/controller/Assert.controller.js
@@ -24,3 +24,28 @@ export const CreateAssert = AsyncHandler(async (req,res)=>{
   });
 });
 
+
+export const CreateSingleAssert = AsyncHandler(async (req,res) => {
+  const data = req.body;
+  await AssertModel.create(data);
+
+  return res.status(StatusCodes.CREATED).json({
+    message:"Data created"
+  });
+});
+
+export const GetAssertData = AsyncHandler(async (req,res) => {
+  const {limit,page} = req.query;
+  const limits = parseInt(limit) || 10;
+  const pages = parseInt(page) || 1;
+  const skip = (pages -1) * limits;
+  const data = await AssertModel.find({}).sort({_id:-1}).skip(skip).limit(limits);
+  return res.status(StatusCodes.OK).json({
+    message:"data",
+    data
+  });
+});
+
+
+
+
