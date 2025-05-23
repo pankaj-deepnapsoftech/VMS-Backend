850a65cfa3b627d0f55f71cd440e69293f7ce113
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-05-23 16:08:02 +0530

    some change

--- src/controller/Assert.controller.js
+++ src/controller/Assert.controller.js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { StatusCodes } from "http-status-codes";
 
+// local imports
 import { AssertModel } from "../models/AssetInventory.model.js";
 import { AsyncHandler } from "../utils/AsyncHandler.js";
 import { NotFoundError } from "../utils/customError.js";
@@ -8,7 +9,7 @@ import { convertKeysToUnderscore } from "../utils/excelSerialToDate.js";
 import { convertExcelToJson } from "../utils/ExcelToJson.js";
 
 
-export const CreateAssert = AsyncHandler(async (req,res)=>{
+export const CreateAssert = AsyncHandler(async (req, res) => {
   const id = req.currentUser?._id;
   const file = req.file;
   if (!file) {
@@ -24,28 +25,53 @@ export const CreateAssert = AsyncHandler(async (req,res)=>{
   });
 });
 
-
-export const CreateSingleAssert = AsyncHandler(async (req,res) => {
+export const CreateSingleAssert = AsyncHandler(async (req, res) => {
   const data = req.body;
   await AssertModel.create(data);
 
   return res.status(StatusCodes.CREATED).json({
-    message:"Data created"
+    message: "Data created"
   });
 });
 
-export const GetAssertData = AsyncHandler(async (req,res) => {
-  const {limit,page} = req.query;
+export const GetAssertData = AsyncHandler(async (req, res) => {
+  const { limit, page } = req.query;
   const limits = parseInt(limit) || 10;
   const pages = parseInt(page) || 1;
-  const skip = (pages -1) * limits;
-  const data = await AssertModel.find({}).sort({_id:-1}).skip(skip).limit(limits);
+  const skip = (pages - 1) * limits;
+  const data = await AssertModel.find({}).sort({ _id: -1 }).skip(skip).limit(limits);
   return res.status(StatusCodes.OK).json({
-    message:"data",
+    message: "data",
     data
   });
 });
 
+export const UpdateAssertData = AsyncHandler(async (req, res) => {
+  const data = req.body;
+  const { id } = req.params;
+  const find = await AssertModel.findById(id);
+  if (!find) {
+    throw new NotFoundError("Data Not Found", "UpdateAssertData");
+  };
+  await AssertModel.findByIdAndUpdate(id, data);
+  return res.status(StatusCodes.OK).json({
+    message: "data updated Successful"
+  });
+});
+
+export const DeleteAssertData = AsyncHandler(async (req, res) => {
+  const { id } = req.body;
+  const find = await AssertModel.findById(id);
+  if (!find) {
+    throw new NotFoundError("Data is Already deleted", "UpdateAssertData");
+  };
+
+  await AssertModel.findByIdAndDelete(id);
+
+  return res.status(StatusCodes.OK).json({
+    message:"Data deleted Successful"
+  });
+});
 
 
 
