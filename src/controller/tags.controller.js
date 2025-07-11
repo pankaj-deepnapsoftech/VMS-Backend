b86d9616567812fea6d9417e28b13321d62e3cd9
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-07-11 16:17:32 +0530

    some change

--- src/controller/tags.controller.js
+++ src/controller/tags.controller.js
@@ -52,6 +52,15 @@ export const UpdateTag = AsyncHandler(async (req, res) => {
 });
 
 
+export const AllTag = AsyncHandler(async (req, res) => {
+  const tags = await TagModel.find({}).select("tag_color tag_name");
+  return res.status(200).json({
+    message: "Tags fetched successfully",
+    data: tags,
+  });
+});
+
+
 
 
 
--- src/routes/Tags.routes.js
+++ src/routes/Tags.routes.js
@@ -1,5 +1,5 @@
 import Router from 'express';
-import { CraeteTags, deleteTag, GetAllTags, UpdateTag } from '../controller/tags.controller.js';
+import { AllTag, CraeteTags, deleteTag, GetAllTags, UpdateTag } from '../controller/tags.controller.js';
 import { Validater } from '../helper/checkValidation.js';
 import { TagValidation } from '../validation/Tages.validation.js';
 
@@ -10,6 +10,7 @@ router.route("/create").post(Validater(TagValidation),CraeteTags);
 router.route("/get-tags").get(GetAllTags);
 router.route("/delete/:id").delete(deleteTag);
 router.route("/update-tag/:id").put(UpdateTag);
+router.route("/get-all-tags").get(AllTag);
 
 
 
